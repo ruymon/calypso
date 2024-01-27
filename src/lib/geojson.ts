b4df955a4f9d6d44a3a80f109feb0597ab2bce46
Ruy Monteiro <ruystfrancis@gmail.com>
2024-01-27 00:27:38 -0300

    Remove console.log

--- src/lib/geojson.ts
+++ src/lib/geojson.ts
@@ -5,11 +5,6 @@ import { point } from "@turf/helpers";
 export function liveFlightToGeoJson(data: LiveFlight[]): GeoJSON.FeatureCollection<GeoJSON.Geometry> {
   const features = data.map((flight) => point(flight.currentPosition.coordinates, flight, { id: flight.id }))
 
-  console.log({
-    type: "FeatureCollection",
-    features,
-  })
-  
   return {
     type: "FeatureCollection",
     features,
