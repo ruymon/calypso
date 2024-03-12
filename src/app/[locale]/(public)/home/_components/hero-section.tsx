d2a94080919b68188ffa14da832bf076dff32f9a
Ruy Monteiro <ruystfrancis@gmail.com>
2024-03-12 13:31:59 -0300

    fix(lint): Hero section)

--- src/app/[locale]/(public)/home/_components/hero-section.tsx
+++ src/app/[locale]/(public)/home/_components/hero-section.tsx
@@ -11,21 +11,21 @@ export async function HeroSection({}: HeroSectionProps) {
   const t = await getScopedI18n("landing.hero");
   return (
     <div className="flex min-h-[calc(100vh-5rem)] flex-col items-center justify-center py-8">
-      <header className="flex w-full lg:max-w-2xl flex-col items-center justify-center gap-3 text-center">
-        <span className="mb-3 rounded bg-gradient-to-r from-violet-500 to-rose-400 p-0.5 px-2 font-mono text-xs md:text-sm font-black uppercase text-white">
+      <header className="flex w-full flex-col items-center justify-center gap-3 text-center lg:max-w-2xl">
+        <span className="mb-3 rounded bg-gradient-to-r from-violet-500 to-rose-400 p-0.5 px-2 font-mono text-xs font-black uppercase text-white md:text-sm">
           {t("hat")}
         </span>
 
         <Balancer
           as="h1"
-          className="text-center font-black text-foreground text-6xl lg:text-7xl"
+          className="text-center text-6xl font-black text-foreground lg:text-7xl"
         >
           {t("title")}
         </Balancer>
 
         <Balancer
           as="span"
-          className="md:w-3/4 text-center text-base md:text-xl leading-relaxed text-muted-foreground"
+          className="text-center text-base leading-relaxed text-muted-foreground md:w-3/4 md:text-xl"
         >
           {t("subtitle")}
         </Balancer>
@@ -43,7 +43,7 @@ export async function HeroSection({}: HeroSectionProps) {
         </Link>
       </header>
 
-      <figure className="flex flex-col items-center justify-center gap-4 mt-8">
+      <figure className="mt-8 flex flex-col items-center justify-center gap-4">
         <span className="max-w-32 text-center font-mono text-xs font-bold uppercase text-muted-foreground">
           {t("scroll")}
         </span>
