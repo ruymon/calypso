87cec1913438ef1370fe6d0c37d67fd3e0aab5a0
Ruy Monteiro <ruystfrancis@gmail.com>
2024-04-23 10:13:14 -0300

    fix(config): Update email

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ export const siteConfig = {
   description:
     "Elevate your flight simulation experience with our innovative online map tool. Monitor live connections across multiple flight simulation networks, fostering real-time interaction among virtual aviators. Explore dynamic features and stay tuned for new enhancements, making Skyscope the go-to platform for a more immersive and collaborative community.",
   slogan: "Supercharge your flights",
-  email: "support@skyscope.app",
+  email: "skyscopeapp@gmail.com",
   links: {
     discord: "https://discord.gg/gSmAuzhzgR",
   },
