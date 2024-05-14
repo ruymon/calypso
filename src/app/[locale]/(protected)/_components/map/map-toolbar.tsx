77980b7f986490c5d789554c0a53bee777d7a6fa
Ruy Monteiro <ruystfrancis@gmail.com>
2024-05-14 08:53:58 -0300

    fix(map): Hide wip filter

--- src/app/[locale]/(protected)/_components/map/map-toolbar.tsx
+++ src/app/[locale]/(protected)/_components/map/map-toolbar.tsx
@@ -1,5 +1,4 @@
 import { MapBaseMapControls } from "./map-base-map-controls";
-import { MapFilterControls } from "./map-filter-controls";
 import { MapLayerControls } from "./map-layer-controls";
 
 interface MapToolbarProps {}
@@ -9,7 +8,7 @@ export function MapToolbar({}: MapToolbarProps) {
     <div className="absolute right-3 top-3 z-10 flex flex-col items-center justify-center gap-0.5 rounded-lg border bg-popover p-1 text-popover-foreground backdrop-blur-lg">
       <MapBaseMapControls />
       <MapLayerControls />
-      <MapFilterControls />
+      {/* <MapFilterControls /> */}
     </div>
   );
 }
