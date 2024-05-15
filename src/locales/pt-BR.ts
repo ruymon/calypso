534e709fce86350e13f461f563ecb68415b093e1
Ruy Monteiro <ruystfrancis@gmail.com>
2024-05-15 01:14:03 -0300

    New translations en.ts (Portuguese, Brazilian)

--- src/locales/pt-BR.ts
+++ src/locales/pt-BR.ts
@@ -152,8 +152,8 @@ export default ({
       },
       extraLayers: {
         tooltip: {
-          title: "Extra layers",
-          description: "Toggle extra layers visibility"
+          title: "Camadas das redes",
+          description: "Gerir a visibilidade dos dados das redes"
         },
         title: "Extra layers",
         airports: {
