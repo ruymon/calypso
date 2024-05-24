47ae6fc7f78fb47733937faebf366e4066a98914
Ruy Monteiro <ruystfrancis@gmail.com>
2024-05-24 10:52:12 -0300

    feat(layers): Persist state

--- src/stores/map-extra-layers-store.ts
+++ src/stores/map-extra-layers-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export interface MapExtraLayersStore {
   isAirportsLayerVisible: boolean;
@@ -7,11 +8,18 @@ export interface MapExtraLayersStore {
   setIsWeatherLayerVisible: (visible: boolean) => void;
 }
 
-export const useMapExtraLayersStore = create<MapExtraLayersStore>((set) => ({
-  isAirportsLayerVisible: false,
-  setIsAirportsLayerVisible: (visible: boolean) =>
-    set({ isAirportsLayerVisible: visible }),
-  isWeatherLayerVisible: false,
-  setIsWeatherLayerVisible: (visible: boolean) =>
-    set({ isWeatherLayerVisible: visible }),
-}));
+export const useMapExtraLayersStore = create(
+  persist<MapExtraLayersStore>(
+    (set) => ({
+      isAirportsLayerVisible: false,
+      setIsAirportsLayerVisible: (visible: boolean) =>
+        set({ isAirportsLayerVisible: visible }),
+      isWeatherLayerVisible: false,
+      setIsWeatherLayerVisible: (visible: boolean) =>
+        set({ isWeatherLayerVisible: visible }),
+    }),
+    {
+      name: "map-extra-layers-store",
+    },
+  ),
+);
--- src/stores/map-network-layers-store.ts
+++ src/stores/map-network-layers-store.ts
@@ -1,4 +1,5 @@
 import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 export interface MapNetworkLayersStore {
   isVatsimFlightsLayerVisible: boolean;
@@ -13,22 +14,27 @@ export interface MapNetworkLayersStore {
   setIsAirportsLayerVisible: (visible: boolean) => void;
 }
 
-export const useMapNetworkLayersStore = create<MapNetworkLayersStore>(
-  (set) => ({
-    isVatsimFlightsLayerVisible: true,
-    setIsVatsimFlightsLayerVisible: (visible: boolean) =>
-      set({ isVatsimFlightsLayerVisible: visible }),
-    isIvaoFlightsLayerVisible: true,
-    setIsIvaoFlightsLayerVisible: (visible: boolean) =>
-      set({ isIvaoFlightsLayerVisible: visible }),
-    isVatsimATCsLayerVisible: true,
-    setIsVatsimATCsLayerVisible: (visible: boolean) =>
-      set({ isVatsimATCsLayerVisible: visible }),
-    isIvaoATCsLayerVisible: true,
-    setIsIvaoATCsLayerVisible: (visible: boolean) =>
-      set({ isIvaoATCsLayerVisible: visible }),
-    isAirportsLayerVisible: false,
-    setIsAirportsLayerVisible: (visible: boolean) =>
-      set({ isAirportsLayerVisible: visible }),
-  }),
+export const useMapNetworkLayersStore = create(
+  persist<MapNetworkLayersStore>(
+    (set) => ({
+      isVatsimFlightsLayerVisible: true,
+      setIsVatsimFlightsLayerVisible: (visible: boolean) =>
+        set({ isVatsimFlightsLayerVisible: visible }),
+      isIvaoFlightsLayerVisible: true,
+      setIsIvaoFlightsLayerVisible: (visible: boolean) =>
+        set({ isIvaoFlightsLayerVisible: visible }),
+      isVatsimATCsLayerVisible: true,
+      setIsVatsimATCsLayerVisible: (visible: boolean) =>
+        set({ isVatsimATCsLayerVisible: visible }),
+      isIvaoATCsLayerVisible: true,
+      setIsIvaoATCsLayerVisible: (visible: boolean) =>
+        set({ isIvaoATCsLayerVisible: visible }),
+      isAirportsLayerVisible: false,
+      setIsAirportsLayerVisible: (visible: boolean) =>
+        set({ isAirportsLayerVisible: visible }),
+    }),
+    {
+      name: "map-network-layers-store",
+    },
+  ),
 );
