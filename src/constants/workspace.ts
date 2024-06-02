3a3753f2b95a05237f3f8147024177c9175062e0
Ruy Monteiro <ruystfrancis@gmail.com>
2024-06-02 16:48:20 -0300

    fix: deploy version

--- src/constants/workspace.ts
+++ src/constants/workspace.ts
@@ -1,4 +1,4 @@
 import { env } from "@/env.mjs";
 
 export const IS_IN_DEVELOPMENT = env.NEXT_PUBLIC_NODE_ENV === "development";
-export const BUILD_VERSION = "v1.44.2 beta";
+export const BUILD_VERSION = "v1.44.3 beta";
